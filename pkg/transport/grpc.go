5e2470916d0d2ee37b4c30f919778e46813ab872
Konstantin Wirz <konstantin.wirz@protonmail.com>
2021-12-13 11:29:35 +0100

    Rename Users service to UserService

--- pkg/transport/grpc.go
+++ pkg/transport/grpc.go
@@ -2,9 +2,9 @@ package transport
 
 import "github.com/status-owl/user-service/pb"
 
-//go:generate protoc --go_out=../../pb --go-grpc_out=../../pb  --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --proto_path=../../pb ../../pb/users.proto
-//go:generate mockgen -source ../../pb/users_grpc.pb.go -destination ../../pb/users_grpc_mock.pb.go -package pb
+//go:generate protoc --go_out=../../pb --go-grpc_out=../../pb  --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --proto_path=../../pb ../../pb/usersvc.proto
+//go:generate mockgen -source ../../pb/usersvc_grpc.pb.go -destination ../../pb/usersvc_grpc_mock.pb.go -package pb
 
 type grpcServer struct {
-	pb.UnimplementedUsersServer
+	pb.UnimplementedUserServiceServer
 }
